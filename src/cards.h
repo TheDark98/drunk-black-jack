c7de55825d5397fae996cb0c61b92cb93da85682
TheDark98 <dark98cooperation@gmail.com>
2026-02-12 20:49:36 +0100

    added
    cards

---
+++ src/cards.h
@@ -0,0 +1,22 @@
+enum cardSeed {
+    Heart,
+    Diamond,
+    Spade,
+    Club
+};
+
+enum card {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+};
