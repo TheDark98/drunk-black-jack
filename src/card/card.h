714c4afae6c2bbfdf921067ffc869627eca8dafc
TheDark98 <dark98cooperation@gmail.com>
2026-02-13 16:46:53 +0100

    updated card

--- src/card/card.h
+++ src/card/card.h
@@ -1,8 +1,16 @@
 #pragma once
 
 #include "cards_type.h"
+#include <string>
+#include <array>
 
 namespace DrunkEngine {
+
+    constexpr uint8_t FromEnumToInt(const DrunkEngine::CardType::Value value);
+    constexpr uint8_t FromEnumToInt(const DrunkEngine::CardType::Seed seed);
+    constexpr std::string_view FromEnumToString(const DrunkEngine::CardType::Value value);
+    constexpr std::string_view FromEnumToString(const DrunkEngine::CardType::Seed seed);
+
     struct Card {
         DrunkEngine::CardType::Value value = DrunkEngine::CardType::Value::UNDEFINED;
         DrunkEngine::CardType::Seed seed = DrunkEngine::CardType::Seed::UNDEFINED;
@@ -10,5 +18,43 @@ namespace DrunkEngine {
         bool operator==(const Card other) const {
             return this->value == other.value && seed == other.seed;
         }
+
+        uint8_t operator+(const Card other) const {
+            return FromEnumToInt(this->value) + FromEnumToInt(other.value);
+        }
     };
+
+    constexpr uint8_t FromEnumToInt(const DrunkEngine::CardType::Value value) {
+        if (value == DrunkEngine::CardType::Value::UNDEFINED) return 0;
+        if (value >= DrunkEngine::CardType::Value::JACK) return 10;
+        return static_cast<uint8_t>(value);
+    }
+
+    constexpr uint8_t FromEnumToInt(const DrunkEngine::CardType::Seed seed) {
+        if (seed == DrunkEngine::CardType::Seed::UNDEFINED) return 0;
+        return static_cast<uint8_t>(seed);
+    }
+
+    constexpr std::string_view FromEnumToString(const DrunkEngine::CardType::Value value) {
+        return DrunkEngine::CardType::ValueName[FromEnumToInt(value)];
+    }
+
+    constexpr std::string_view FromEnumToString(const DrunkEngine::CardType::Seed seed) {
+        return DrunkEngine::CardType::SeedName[FromEnumToInt(seed)];
+    }
+
+    constexpr std::string FromCardToString(const DrunkEngine::Card card) {
+        return std::string(FromEnumToString(card.value)) + " | " + std::string(FromEnumToString(card.seed).data());
+    }
+
+    constexpr std::string FromCardToString(const std::array<DrunkEngine::Card, 11> &cards) {
+        std::string handString;
+        handString.reserve(256);
+
+        for (uint8_t i = 0; i < cards.size() && cards[i].value != DrunkEngine::CardType::Value::UNDEFINED; i++) {
+            if (i > 0) handString += "\n";
+            handString += FromCardToString(cards[i]);
+        }
+        return handString;
+    }
 }
--- src/card/cards_type.h
+++ src/card/cards_type.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <string_view>
+#include <cstdint>
+
 namespace DrunkEngine
 {
     namespace CardType
     {
+        constexpr std::string_view SeedName[5] = {
+            "Undefined",
+            "Heart",
+            "Diamond",
+            "Spade",
+            "Club"
+        };
 
         enum class Seed
         {
@@ -13,7 +23,24 @@ namespace DrunkEngine
             SPADE,
             CLUB
         };
-
+        
+        constexpr std::string_view ValueName[14] = {
+            "Undefined",
+            "Ace",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+            "Jack",
+            "Queen",
+            "King"
+        };
+        
         enum class Value
         {
             UNDEFINED,
@@ -27,9 +54,9 @@ namespace DrunkEngine
             EIGHT,
             NINE,
             TEN,
-            JACK = 10,
-            QUEEN = 10,
-            KING = 10
+            JACK,
+            QUEEN,
+            KING
         };
     }
 }
